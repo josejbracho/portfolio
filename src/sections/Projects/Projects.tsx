f002b081a5dd808d90fac20d95e604257e3f3afe
Jose Bracho <josejbracho@gmail.com>
2025-01-26 09:55:51 -0500

    fix: projects coming soon

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,9 +1,9 @@
 import styles from "./ProjectsStyles.module.css";
-import viberr from "../../assets/viberr.png";
-import freshBurger from "../../assets/fresh-burger.png";
-import hipsster from "../../assets/hipsster.png";
-import fitLift from "../../assets/fitlift.png";
-import ProjectCard from "../../common/ProjectCard";
+// import viberr from "../../assets/viberr.png";
+// import freshBurger from "../../assets/fresh-burger.png";
+// import hipsster from "../../assets/hipsster.png";
+// import fitLift from "../../assets/fitlift.png";
+// import ProjectCard from "../../common/ProjectCard";
 
 // TODO: add projects
 
@@ -12,7 +12,7 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard src={viberr} link="" h3="Viberr" p="Streaming App" />
+        {/* <ProjectCard src={viberr} link="" h3="Viberr" p="Streaming App" />
         <ProjectCard
           src={freshBurger}
           link=""
@@ -20,7 +20,8 @@ function Projects() {
           p="Hamburger Restaurant"
         />
         <ProjectCard src={hipsster} link="" h3="Hipsster" p="Glasses Shop" />
-        <ProjectCard src={fitLift} link="" h3="FitLift" p="Fitness App" />
+        <ProjectCard src={fitLift} link="" h3="FitLift" p="Fitness App" /> */}
+        <p className={styles.description}>Coming soon! 🚧</p>
       </div>
     </section>
   );
